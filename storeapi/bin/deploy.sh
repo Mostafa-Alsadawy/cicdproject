99c2161d4760658caf812c0d5b5156ee6c3039a2
Mostafa-Alsadawy <mostafa.alsadawy2021@gmail.com>
2022-05-22 03:59:02 +0200

    lkj

--- storeapi/bin/deploy.sh
+++ storeapi/bin/deploy.sh
@@ -13,6 +13,6 @@ eb deploy
 # echo "final step add enviromental variables"
 # eb setenv NODE_ENV=dev HOST=database-1.cc5q73rknskb.us-east-1.rds.amazonaws.com DB_PORT=5432 DATABASE=postgres DATABASE_TEST=store_test DB_USER=postgres PASSWORD=Aa123456 PEPPER=secretpass SALT_ROUNDs=12 TOKEN_SECRET=topsecret
 
-for circle ci
+#for circle ci
 echo "final step add enviromental variables"
 eb setenv NODE_ENV=$NODE_ENV HOST=$HOST DB_PORT=$DB_PORT DATABASE=$DATABASE DATABASE_TEST=$DATABASE_TEST DB_USER=$DB_USER PASSWORD=$PASSWORD PEPPER=$PEPPER SALT_ROUNDs=$SALT_ROUNDs TOKEN_SECRET=$TOKEN_SECRET
