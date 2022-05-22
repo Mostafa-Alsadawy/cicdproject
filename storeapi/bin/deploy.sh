5d83c3d7cd7fe0d21de03369f25c443db475ab56
Mostafa-Alsadawy <mostafa.alsadawy2021@gmail.com>
2022-05-22 03:55:26 +0200

    fix:issue on shell script

--- storeapi/bin/deploy.sh
+++ storeapi/bin/deploy.sh
@@ -9,10 +9,10 @@ cd ./.elasticbeanstalk && echo -e "deploy: \n   artifact: ./www/Archive.zip" >> 
 echo "4- Forth step deploy application"
 eb deploy
 
-For local enviroment
-echo "final step add enviromental variables"
-eb setenv NODE_ENV=dev HOST=database-1.cc5q73rknskb.us-east-1.rds.amazonaws.com DB_PORT=5432 DATABASE=postgres DATABASE_TEST=store_test DB_USER=postgres PASSWORD=Aa123456 PEPPER=secretpass SALT_ROUNDs=12 TOKEN_SECRET=topsecret
-
-# for circle ci
+# For local enviroment
 # echo "final step add enviromental variables"
-# eb setenv NODE_ENV=$NODE_ENV HOST=$HOST DB_PORT=$DB_PORT DATABASE=$DATABASE DATABASE_TEST=$DATABASE_TEST DB_USER=$DB_USER PASSWORD=$PASSWORD PEPPER=$PEPPER SALT_ROUNDs=$SALT_ROUNDs TOKEN_SECRET=$TOKEN_SECRET
+# eb setenv NODE_ENV=dev HOST=database-1.cc5q73rknskb.us-east-1.rds.amazonaws.com DB_PORT=5432 DATABASE=postgres DATABASE_TEST=store_test DB_USER=postgres PASSWORD=Aa123456 PEPPER=secretpass SALT_ROUNDs=12 TOKEN_SECRET=topsecret
+
+for circle ci
+echo "final step add enviromental variables"
+eb setenv NODE_ENV=$NODE_ENV HOST=$HOST DB_PORT=$DB_PORT DATABASE=$DATABASE DATABASE_TEST=$DATABASE_TEST DB_USER=$DB_USER PASSWORD=$PASSWORD PEPPER=$PEPPER SALT_ROUNDs=$SALT_ROUNDs TOKEN_SECRET=$TOKEN_SECRET
